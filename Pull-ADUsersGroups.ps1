4fe02e00c8330b109988d6bf2aefab7f39306092
Rob Waight <43173714+rwaight@users.noreply.github.com>
2018-10-24 13:37:20 -0500

    Clean up code and comments

--- Pull-ADUsersGroups.ps1
+++ Pull-ADUsersGroups.ps1
@@ -1,39 +1,37 @@
 # -----------------------------------------------------------------------------
-# Script: Pull-ADUsersGroups.ps1
-# Author: Robert Waight
-# Date: 01/21/2016
-# Keywords: 
-# comments: 
+# Script  : Pull-ADUsersGroups.ps1
+# Author  : Robert Waight
+# Date    : 01/21/2016
+# Keywords: Active Directory, User reporting, Group reporting
+# Comments: Run this in a scheduled task to generate auditable AD User data
 #
 # -----------------------------------------------------------------------------
 
-function Pull-ADUsersGroups{
-
+Function Pull-ADUsersGroups{
 <#
 .DESCRIPTION
     Gets all the users group information from Active Directory and outputs specified properties to a csv
 
 .EXAMPLE
     .\Pull-ADUsersGroups.ps1
-
 #>
 
-Import-Module ActiveDirectory
-#$datetime=Get-Date -format "yyyyMMddTHHmmss"
-$datetime=Get-Date -format "yyyyMMdd"
-$OutFile="C:\PowerShell\ADAutomation\ADUsers-Groups_$datetime.csv"
+  Import-Module ActiveDirectory
+  #$datetime=Get-Date -format "yyyyMMddTHHmmss"
+  $datetime=Get-Date -format "yyyyMMdd"
+  $OutFile="C:\PowerShell\ADAutomation\ADUsers-Groups_$datetime.csv"
 
-$userGrp=Get-ADUser -Filter * -Properties SamAccountName,DisplayName,MemberOf,Enabled | % {
-  New-Object PSObject -Property @{
-	SamAccount = $_.SamAccountName
-    Enabled = $_.Enabled
-	UserName = $_.DisplayName
-	Groups = ($_.MemberOf | Get-ADGroup | Select -ExpandProperty Name) -join ";"
-	}
-} | Select SamAccount,UserName,Enabled,Groups
+  $userGrp=Get-ADUser -Filter * -Properties SamAccountName,DisplayName,MemberOf,Enabled | % {
+    New-Object PSObject -Property @{
+      SamAccount = $_.SamAccountName
+      Enabled = $_.Enabled
+      UserName = $_.DisplayName
+      Groups = ($_.MemberOf | Get-ADGroup | Select -ExpandProperty Name) -join ";"
+    } # end of: New-Object PSObject
+  } | Select SamAccount,UserName,Enabled,Groups # end of: $userGrp=Get-ADUser
 
-$userGrp | Export-CSV $OutFile -NoTypeInformation
-Write-Host -ForegroundColor Cyan "Pull-ADUsersGroups finished!"
-}
+  $userGrp | Export-CSV $OutFile -NoTypeInformation
+  Write-Host -ForegroundColor Cyan "Pull-ADUsersGroups finished!"
+} # end of: Function Pull-ADUsersGroups
 
 Pull-ADUsersGroups
